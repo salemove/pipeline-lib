b8026c9a2c339c4ecba134af5c861bb829113d62
Vlad Nikiforov <vlad@salemove.com>
2019-12-03 16:26:41 +0200

    Remove incorrectly declared functions
    INF-2074
    
    Forgot that functions can only be declared in global scope or in a class,
    otherwise they should be closures. Removed functions altogether, as the flow
    is pretty straightforward anyway.

--- vars/buildWithResults.groovy
+++ vars/buildWithResults.groovy
@@ -2,15 +2,12 @@ import groovy.json.JsonSlurperClassic
 
 def call(Map args = [:]) {
 
-  def getAnsibleVars(buildResult) {
-    def buildVars = buildResult.getBuildVariables()
-    def ansibleVarsJson = buildVars.ansibleVarsJson
-    assert ansibleVarsJson: "No results found - make sure to call ansiblePlaybookWithResults"
-    def slurper = new JsonSlurperClassic()
-    return slurper.parseText(ansibleVarsJson)
-  }
-
   def buildResult = build(args)
-  def ansibleVars = getAnsibleVars(buildResult)
+  def buildVars = buildResult.getBuildVariables()
+  def ansibleVarsJson = buildVars.ansibleVarsJson
+  assert ansibleVarsJson: "No results found - make sure to call ansiblePlaybookWithResults"
+
+  def slurper = new JsonSlurperClassic()
+  def ansibleVars = slurper.parseText(ansibleVarsJson)
   return [buildResult, ansibleVars]
 }
