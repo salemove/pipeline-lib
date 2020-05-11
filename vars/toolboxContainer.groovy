02de35ed0b06b0a5a8f3239a4b4619cea1f6c8ec
Vladimir Kuznichenkov <vladimir.kuznichenkov@glia.com>
2020-05-11 18:43:49 +0300

    Use new toolbox with Terraform
    Related [PR][1] in toolbox repo
    
    [1]: https://github.com/salemove/jenkins-toolbox/pull/16

--- vars/toolboxContainer.groovy
+++ vars/toolboxContainer.groovy
@@ -1,7 +1,7 @@
 def call(Map args = [:]) {
   def defaultArgs = [
     name: 'toolbox',
-    image: 'salemove/jenkins-toolbox:a99ffb7'
+    image: 'salemove/jenkins-toolbox:769f4eb'
   ]
 
   interactiveContainer(defaultArgs << args)
