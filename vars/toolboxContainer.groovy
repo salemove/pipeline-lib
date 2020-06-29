813ef6b823931c4061baabaf985dc5f768588bee
Vladimir Kuznichenkov <vladimir.kuznichenkov@glia.com>
2020-06-29 21:10:05 +0300

    Add kdiff helper to print the k8s diff to toolbox
    https://github.com/salemove/jenkins-toolbox/pull/20
    INF-2218
    
    Signed-off-by: Vladimir Kuznichenkov <vladimir.kuznichenkov@glia.com>

--- vars/toolboxContainer.groovy
+++ vars/toolboxContainer.groovy
@@ -1,7 +1,7 @@
 def call(Map args = [:]) {
   def defaultArgs = [
     name: 'toolbox',
-    image: 'salemove/jenkins-toolbox:a99ffb7'
+    image: 'salemove/jenkins-toolbox:9ac7119'
   ]
 
   interactiveContainer(defaultArgs << args)
